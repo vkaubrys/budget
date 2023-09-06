1266af117cfae1204b84b7ca313e13e904ef5418
Vitalis <vkaubrys@gmail.com>
2023-09-06 20:31:56 +0300

    Add Reositories

---
+++ src/main/java/lt/vitalis/budget/repository/IslaidosRepository.java
@@ -0,0 +1,10 @@
+package lt.vitalis.budget.repository;
+
+import lt.vitalis.budget.repository.model.Islaidos;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IslaidosRepository extends CrudRepository<Islaidos, Integer> {
+
+
+
+}
---
+++ src/main/java/lt/vitalis/budget/repository/PajamosRepository.java
@@ -0,0 +1,8 @@
+package lt.vitalis.budget.repository;
+
+import lt.vitalis.budget.repository.model.Pajamos;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PajamosRepository extends CrudRepository<Pajamos, Integer> {
+
+}
