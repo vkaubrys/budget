ba7b7d61b0eb58d317c6be35f9e74f17dabadc4e
Vitalis <vkaubrys@gmail.com>
2023-09-06 20:02:05 +0300

    Initial commit

---
+++ src/test/java/lt/vitalis/budget/BudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vitalis.budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
