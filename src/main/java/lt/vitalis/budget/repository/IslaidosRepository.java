00051d63470d67a939de9bdd71ed081ccecd9382
Vitalis <vkaubrys@gmail.com>
2023-09-07 21:24:31 +0300

    Add IslaidosRepository class

--- src/main/java/lt/vitalis/budget/repository/IslaidosRepository.java
+++ src/main/java/lt/vitalis/budget/repository/IslaidosRepository.java
@@ -1,10 +1,18 @@
 package lt.vitalis.budget.repository;
 
 import lt.vitalis.budget.repository.model.Islaidos;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface IslaidosRepository extends CrudRepository<Islaidos, Integer> {
+
+//    @Query(
+//            value = "SELECT * FROM islaidos WHERE pavadinimas LIKE :name",
+//            nativeQuery = true
+//    )
+//
+//    List<Islaidos> getIslaidosByNameLike(String name);
 
 }
