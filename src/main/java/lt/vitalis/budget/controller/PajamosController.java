f665b866b6ef8af2550b634cc74d3f27ed02d533
Vitalis <vkaubrys@gmail.com>
2023-09-07 21:21:37 +0300

    Add PajamosController

--- src/main/java/lt/vitalis/budget/controller/PajamosController.java
+++ src/main/java/lt/vitalis/budget/controller/PajamosController.java
@@ -1,14 +1,15 @@
 package lt.vitalis.budget.controller;
 
+import lt.vitalis.budget.repository.model.Islaidos;
 import lt.vitalis.budget.repository.model.Pajamos;
 import lt.vitalis.budget.service.PajamosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @Controller
 @RequestMapping(path = "/pajamos")
 // http://localhost:8080/pajamos
@@ -19,7 +20,29 @@ public class PajamosController {
 
     // http://localhost:8080/pajamos/all
     @GetMapping(path = "/all")
-    public @ResponseBody List<Pajamos> generateDummyPajamos(){
+    public @ResponseBody List<Pajamos> generateDummyPajamos() {
         return pajamosService.getAll();
     }
+
+    @RequestMapping(value = "/save", method = RequestMethod.GET)
+    public String savePajamos(Model model){
+        model.addAttribute("key_pajamos", new Pajamos());
+        return "pajamos_create";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String savePajamos(Model model, @ModelAttribute(value = "key_pajamos") Pajamos pajamos) {
+        pajamosService.save(pajamos);
+        model.addAttribute("key_pajamos", pajamos);
+        return "pajamos_create";
+    }
+
+    @GetMapping(value = "/id/{id}")
+    public String getPajamos(Model model, @PathVariable int id){
+        Pajamos pajamos = pajamosService.getPajamosById(id);
+        model.addAttribute("key_pajamos", pajamos);
+        return "pajamos_create";
+    }
+
+
 }
