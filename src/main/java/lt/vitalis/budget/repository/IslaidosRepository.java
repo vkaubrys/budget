32f14f9e4f1523d6c7eeaf935040079f27350cdf
Vitalis <vkaubrys@gmail.com>
2023-09-12 00:26:41 +0300

    Add Islaidos Between Dates

--- src/main/java/lt/vitalis/budget/repository/IslaidosRepository.java
+++ src/main/java/lt/vitalis/budget/repository/IslaidosRepository.java
@@ -4,15 +4,15 @@ import lt.vitalis.budget.repository.model.Islaidos;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface IslaidosRepository extends CrudRepository<Islaidos, Integer> {
 
-//    @Query(
-//            value = "SELECT * FROM islaidos WHERE pavadinimas LIKE :name",
-//            nativeQuery = true
-//    )
-//
-//    List<Islaidos> getIslaidosByNameLike(String name);
+    @Query(
+            value = "select * from islaidos where data between :startDate and :endDate",
+            nativeQuery = true
+    )
 
+    List<Islaidos> getIslaidosBetweenDates(LocalDateTime startDate, LocalDateTime endDate);
 }
