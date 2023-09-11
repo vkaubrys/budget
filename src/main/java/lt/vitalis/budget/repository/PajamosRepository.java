6dd9b701228fc19e0633c10a939e712416e85d98
Vitalis <vkaubrys@gmail.com>
2023-09-12 00:19:45 +0300

    Add Query into PajamosRepository

--- src/main/java/lt/vitalis/budget/repository/PajamosRepository.java
+++ src/main/java/lt/vitalis/budget/repository/PajamosRepository.java
@@ -1,8 +1,19 @@
 package lt.vitalis.budget.repository;
 
 import lt.vitalis.budget.repository.model.Pajamos;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PajamosRepository extends CrudRepository<Pajamos, Integer> {
+
+    @Query(
+            value = "select * from pajamos where data between :startDate and :endDate",
+            nativeQuery = true
+    )
+
+    List<Pajamos> getPajamosBetweenDates(LocalDateTime startDate, LocalDateTime endDate);
 
 }
